6d9994c7d631cf08336af5d9ff75b5cfe8bf7fe4
abhi904485 <abhishek9044855265@gmail.com>
2025-03-02 00:49:49 +0530

    Converted fbv to cbv & optimized queries on order page

--- api/manager.py
+++ api/manager.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class OrderManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().prefetch_related('items__product')
+        return super().get_queryset().prefetch_related('items__product','user')
--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
-    path('users', views.user_list, name='users'),
-    path('users/<int:pk>', views.user_detail, name='user-detail'),
-    path('products/', views.product_list, name='product-list', ),
-    path('products/<int:pk>', views.product_detail, name='product-detail', ),
-    path('products/info', views.product_info, name='product-info', ),
-    path('orders/', views.order_list, name='order-list', ),
-    path('orders/<uuid:order_id>', views.order_detail, name='order-detail', ),
+    path('users', views.UserListAPIView.as_view(), name='users'),
+    path('users/<int:user_id>', views.UserRetrieveAPIView.as_view(), name='user-detail'),
+    path('products/', views.ProductListApiView.as_view(), name='product-list', ),
+    path('products/<int:product_id>', views.ProductRetrieveAPIView.as_view(), name='product-detail', ),
+    path('products/info', views.ProductInfoListAPIView.as_view(), name='product-info', ),
+    path('orders/', views.OrderListAPIView.as_view(), name='order-list', ),
+    path('orders/<uuid:order_id>', views.OrderRetrieveAPIView.as_view(), name='order-detail', ),
 ]
