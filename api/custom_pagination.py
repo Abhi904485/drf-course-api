579741eaca1f8ce8f492ca64f48bcb3f3ff85eb3
abhi904485 <abhishek9044855265@gmail.com>
2025-03-17 16:16:37 +0530

    Added Limit Offset Pagination

--- api/custom_pagination.py
+++ api/custom_pagination.py
@@ -1,8 +1,8 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.response import Response
 
 
-class CustomPagination(PageNumberPagination):
+class CustomPageNumberPagination(PageNumberPagination):
     page_size = 1
     page_size_query_param = 'size'
     max_page_size = 10
@@ -15,5 +15,22 @@ class CustomPagination(PageNumberPagination):
                 'previous': self.get_previous_link(),
             },
             'count': self.page.paginator.count,
+            'results': data
+        })
+
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+    offset_query_param = 'offset'
+    limit_query_param = 'limit'
+    max_limit = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+            },
+            'count': self.limit,
             'results': data
         })
--- drf_course/settings.py
+++ drf_course/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'api.custom_pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.custom_pagination.CustomLimitOffsetPagination',
 }
 
 SPECTACULAR_SETTINGS = {
